32857d97800ad33ab0208fbe3b435c2587018528
Ankita Kushwaha <46566985+anku157501@users.noreply.github.com>
2020-08-03 11:28:29 +0530

    Create RandomMethod.java

---
+++ RandomMethod.java
@@ -0,0 +1,8 @@
+import java.lang.Math;
+public class Main
+{
+	public static void main(String[] args) {
+	    //random() method =A static method which generate double type greater than and equal to 0.0 but less than 1.0
+		System.out.println(Math.random());
+	}
+}
