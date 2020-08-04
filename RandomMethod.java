0f1155b917d698d9cb4bee39b112e227144d1ed6
anku157501 <kushwahaankita053@gmail.com>
2020-08-04 20:09:44 +0530

    initial commit

--- RandomMethod.java
+++ RandomMethod.java
@@ -1,5 +1,5 @@
 import java.lang.Math;
-public class Main
+ class Main
 {
 	public static void main(String[] args) {
 	    //random() method =A static method which generate double type greater than and equal to 0.0 but less than 1.0
